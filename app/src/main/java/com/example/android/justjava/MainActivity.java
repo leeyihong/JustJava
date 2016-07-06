092075a24e718540524c181cdae1e34441ff3790
yihong-lco <yihong@lco-creation.com>
2016-07-06 13:02:22 +0800

    using String data type

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -25,7 +25,10 @@ public class MainActivity extends AppCompatActivity {
      */
     public void submitOrder(View view) {
 //        display(quantity);
-        displayPrice(quantity*5);
+        String priceMessage = "Total: $" + (quantity*5) ;
+        priceMessage = priceMessage + " \nThank you!";
+        displayMessage(priceMessage);
+//        displayPrice(quantity*5);
     }
 
     public void increment(View view) {
@@ -53,5 +56,13 @@ public class MainActivity extends AppCompatActivity {
     private void displayPrice(int number) {
         TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
         priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+
+    /**
+     * This method displays the given text on the screen.
+     */
+    private void displayMessage(String message) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(message);
     }
 }
